16e2ccd5cb585a9f10dd67f7c70379495ea36356
Tapasvi Amruthalal <147241661+tapasvi123@users.noreply.github.com>
2023-12-29 17:02:11 +0530

    Update Majority_Element.py
    Self Implementation. Need to check for other otimised implementations.
    
    Space complexity is O(1)
    Sort: O(n^2)

--- Arrays/Majority_Element.py
+++ Arrays/Majority_Element.py
@@ -1,2 +1,33 @@
-#Assume code is here
-#this is the second update -->
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # num=[2,2,1,1,1,2,2]
+
+        nums.sort()
+        l=len(nums)
+        max=nums[0]
+        prev=0
+        count=0
+
+        for a in range(0,l):
+            if nums[a]==max:
+                count+=1
+                prev=count
+                
+            else:
+                count+=1
+                if ((count-prev) > prev):
+                    prev=count-prev
+                    max=nums[a]
+        return max
+
+
+  
+        #  1 2 2 2 2 3 3 3 3 4 4 4 4 4 4 4 4 4 5 5 5 5 
+        
+# [ Time taken: 29 m 4 s ]
+# [ Memory Size: 15.65 MB]
+# [ Run Time: 118ms ]
